1dc1a841f881edb78e3156b879255fa4792dac96
CryptoDappDev <joegmitt@gmail.com>
2021-08-08 23:03:36 -0700

    Parallax Provider Injection

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/loader"
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlasmicRootProvider>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
+    <ParallaxProvider>
+      <PlasmicRootProvider>
+        <Component {...pageProps} />
+      </PlasmicRootProvider>
+    </ParallaxProvider>
   )
 }
 
