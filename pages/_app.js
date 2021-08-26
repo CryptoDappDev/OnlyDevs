a2e514e8844b29d81b39ec363d588ffc7dbbb1b0
CryptoDappDev <joegmitt@gmail.com>
2021-08-25 18:18:03 -0700

    Font Update #6

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,28 @@
+import React from 'react'
 import '../styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/loader"
 import { ParallaxProvider } from 'react-scroll-parallax';
+import Head from "next/head";
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ParallaxProvider>
-      <PlasmicRootProvider>
-        <Component {...pageProps} />
-      </PlasmicRootProvider>
-    </ParallaxProvider>
+    <div>
+      <Head>
+        <link
+          href= "../styles/globals.css"
+          rel="stylesheet"
+          />
+      </Head>
+        <ParallaxProvider>
+          <PlasmicRootProvider>
+            <Component {...pageProps} />
+          </PlasmicRootProvider>
+        </ParallaxProvider>
+    </div>
   )
+    
+    
 }
 
 export default MyApp
