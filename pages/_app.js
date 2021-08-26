4238f382f352fb5f35c26e5873972749f3bf5317
CryptoDappDev <joegmitt@gmail.com>
2021-08-25 18:42:40 -0700

    Font Update #7

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,71 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <link
-          href= "../styles/globals.css"
-          rel="stylesheet"
+        <link rel="preload"
+            href="/fonts/Objectivity-Black.ttf"
+            as="font" crossOrigin="" 
           />
+        <link rel="preload"
+            href="/fonts/Objectivity-BlackSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Bold.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-BlackSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-ExtraBold.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-ExtraBoldSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Light.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-LightSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Medium.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-MediumSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Regular.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-RegularSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Super.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-SuperSlanted.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-Thin.ttf"
+            as="font" crossOrigin="" 
+          />
+        <link rel="preload"
+            href="/fonts/Objectivity-ThinSlanted.ttf"
+            as="font" crossOrigin="" 
+          />       
+        
       </Head>
         <ParallaxProvider>
           <PlasmicRootProvider>
@@ -21,8 +82,7 @@ function MyApp({ Component, pageProps }) {
         </ParallaxProvider>
     </div>
   )
-    
-    
+     
 }
 
 export default MyApp
